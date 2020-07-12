293f0d7d81402592c2b9d9bb9ec6a0d1116849e3
Onyekaba Ekenedilichukwu <ekeneonyekaba@gmail.com>
2020-07-12 10:51:33 +0100

    feat: CRUD on a List of Todos
    I modified the code so that user goes to a homepage for a particular
    todo list rather than seeing all todo items in the database.

--- app.py
+++ app.py
@@ -1,42 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:kenzy@localhost:5432/todoapp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
-    # Set the tablename in the database to todos
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    completed = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey(
+        'todolists.id'), nullable=False)
 
-    # debugging statements
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-"""
-Sync our models with database
-To ensure that tables are created for all models that we have declared and they havent yet been created
-"""
-# db.create_all()
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
-# CREATE OPERATION
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()['description']
+        list_id = request.get_json()['list_id']
         todo = Todo(description=description)
+        active_list = TodoList.query.get(list_id)
+        todo.list = active_list
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
@@ -46,26 +47,12 @@ def create_todo():
         print(sys.exc_info())
     finally:
         db.session.close()
-    if error:
-        abort(400)
-    else:
+    if not error:
         return jsonify(body)
+    else:
+        abort(500)
 
-# UPDATE OPERATION
-@app.route('/todos/<todo_id>/set-completed', methods={'POST'})
-def set_completed_todo(todo_id):
-    try:
-        completed = request.get_json()['completed']
-        todo = Todo.query.get(todo_id)
-        todo.completed = completed
-        db.session.commit()
-    except:
-        db.session.rollback()
-    finally:
-        db.session.close()
-    return redirect(url_for('index'))
 
-# DELETE OPERATION
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     try:
@@ -75,15 +62,34 @@ def delete_todo(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-    # return jsonify({'success': True})
+    return jsonify({'success': True})
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
     return redirect(url_for('index'))
 
 
-# GET OPERATION
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+                           lists=TodoList.query.all(),
+                           active_list=TodoList.query.get(list_id),
+                           todos=Todo.query.filter_by(
+                               list_id=list_id).order_by('id').all()
+                           )
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    return redirect(url_for('get_list_todos', list_id=1))
