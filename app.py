b9be3e650bff7d23f1456c4e8f48bbbfca78abf2
Onyekaba Ekenedilichukwu <ekeneonyekaba@gmail.com>
2020-07-12 05:12:10 +0100

    feat: Flask  Migrations
    Creating a migrations folder and sync our models to the
    database using different commands like :
    flask db init - Creates a migrations folder
    flask db migrate - Detect changes from our models
    flask db upgrade - Add those changes to our database
    flask db downgrade - Revert those changes to our database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -7,12 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:kenzy@localhost:543
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     # Set the tablename in the database to todos
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     # debugging statements
     def __repr__(self):
@@ -23,7 +27,7 @@ class Todo(db.Model):
 Sync our models with database
 To ensure that tables are created for all models that we have declared and they havent yet been created
 """
-db.create_all()
+# db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -42,7 +46,9 @@ def create_todo():
         print(sys.exc_info())
     finally:
         db.session.close()
-    if not error:
+    if error:
+        abort(400)
+    else:
         return jsonify(body)
 
 
@@ -51,5 +57,5 @@ def index():
     return render_template('index.html', data=Todo.query.all())
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
